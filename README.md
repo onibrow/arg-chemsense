6967f7461aab120de75eb19e57ad7838d286d9dc
onibrow <seiya.ono12@gmail.com>
2020-01-29 00:29:20 +0000

    get pyserial to work and request adc data

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Arias Reesearch Group - Chemical Sensors
 
 Quad Channel Differential Potentiometric Sensor
+
+Using the MCP3424 to sense differential voltage - firmware library [here](https://github.com/battosai30/MCP3424)
--- diff/diff.ino
+++ diff/diff.ino
@@ -1,37 +1,44 @@
 /* MCP 3424 version 1.2 example sketch OneShotConversion
-Written by B@tto 
-Contact : batto@hotmail.fr
+  Written by B@tto
+  Contact : batto@hotmail.fr
 
-In this example, one conversion per second is performed on channel 1 and 16 bits resolution. 
-A new conversion has to be initiated by the user
+  In this example, one conversion per second is performed on channel 1 and 16 bits resolution.
+  A new conversion has to be initiated by the user
 */
 
 
 #include <Wire.h>
 #include <MCP342x.h>
 
-MCP342x MCP(0); // Declaration of MCP3424
+MCP342x MCP(0);
 
-long Voltage;
+long Voltage[4];
+char incomingByte = 0;
+int num_samples = 1;
 
-void setup(){ 
-
-  Serial.begin(9600); // start serial for output
+void setup() {
   MCP.begin(0);
-  MCP.setConfiguration(CH1,RESOLUTION_16_BITS,ONE_SHOT_MODE,PGA_X1); // Channel 1, 16 bits resolution, one-shot mode, amplifier gain = 1
-
+  MCP.setConfiguration(CH1, RESOLUTION_16_BITS, CONTINUOUS_MODE, PGA_X1); // Channel 1, 16 bits resolution, one-shot mode, amplifier gain = 1
+  Serial.begin(115200); // start serial for output
+  Serial.print("MCP3424 Configuration Complete!\n");
 }
 
-void loop(){
-
-  MCP.newConversion(); // New conversion is initiated
-  
-  Voltage=MCP.measure(); // Measure, note that the library waits for a complete conversion
- 
-//  Serial.print("Voltage = "); // print result
-  Serial.println(Voltage);
-//  Serial.println(" microVolt");
-  
-  delay (100);
-  
+void loop() {
+  for (int i = 1; i <= 4; i++) {
+    MCP.setConfiguration(i, RESOLUTION_16_BITS, CONTINUOUS_MODE, PGA_X1);
+    long cumsum_voltage = 0;
+    for (int j = 0; j < num_samples; j++) {
+      cumsum_voltage += MCP.measure();
+    }
+    Voltage[i - 1] = -1 * cumsum_voltage / num_samples;
+  }
+  if (Serial.available()) {
+    incomingByte = Serial.read();
+    if (incomingByte == 0x64) {
+      Serial.print(Voltage[0]); Serial.print(",");
+      Serial.print(Voltage[1]); Serial.print(",");
+      Serial.print(Voltage[2]); Serial.print(",");
+      Serial.print(Voltage[3]); Serial.print(",\n");
+    }
+  }
 }
