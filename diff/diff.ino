0b5ac9138d8e13916b7af86b101a6cbfb7356ecd
onibrow <seiya.ono12@gmail.com>
2020-03-13 21:22:12 +0000

    clean up

--- diff/diff.ino
+++ diff/diff.ino
@@ -1,24 +1,24 @@
-/* MCP 3424 version 1.2 example sketch OneShotConversion
-  Written by B@tto
-  Contact : batto@hotmail.fr
-
-  In this example, one conversion per second is performed on channel 1 and 16 bits resolution.
-  A new conversion has to be initiated by the user
+/*
+ * 1999250000 = 2
 */
 
 
 #include <Wire.h>
 #include <MCP342x.h>
 
-MCP342x MCP(0);
+MCP342x MCPA(0);
 
-long Voltage[4];
+MCP342x MCPB(2);
+
+long Voltage[2][4];
 String buff = String("deadbuffdead");
 int num_samples = 1;
 
 void setup() {
-  MCP.begin(0);
-  MCP.setConfiguration(CH1, RESOLUTION_16_BITS, ONE_SHOT_MODE, PGA_X1); // Channel 1, 16 bits resolution, one-shot mode, amplifier gain = 1
+  MCPA.begin(0);
+  MCPB.begin(0);
+  MCPA.setConfiguration(CH1, RESOLUTION_16_BITS, ONE_SHOT_MODE, PGA_X1); // Channel 1, 16 bits resolution, one-shot mode, amplifier gain = 1
+  MCPB.setConfiguration(CH1, RESOLUTION_16_BITS, ONE_SHOT_MODE, PGA_X1);
   Serial.begin(115200); // start serial for output
   //  Serial.print("MCP3424 Configuration Complete!\n");
 }
@@ -26,21 +26,30 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     buff = Serial.readStringUntil('\n');
-//    Serial.println(buff);
+    //    Serial.println(buff);
     for (int i = 1; i <= 4; i++) {
-      MCP.setConfiguration(i, RESOLUTION_16_BITS, ONE_SHOT_MODE, PGA_X1);
-      long cumsum_voltage = 0;
+      MCPA.setConfiguration((CHANNELS) i, RESOLUTION_16_BITS, ONE_SHOT_MODE, PGA_X1);
+      MCPB.setConfiguration((CHANNELS) i, RESOLUTION_16_BITS, ONE_SHOT_MODE, PGA_X1);
+      long cumsum_voltage_A = 0;
+      long cumsum_voltage_B = 0;
       for (int j = 0; j < num_samples; j++) {
-        MCP.newConversion();
-        cumsum_voltage += MCP.measure();
+        MCPA.newConversion();
+        MCPB.newConversion();
+        cumsum_voltage_A += MCPA.measure();
+        cumsum_voltage_B += MCPB.measure();
       }
-      Voltage[i - 1] = -1 * cumsum_voltage / num_samples;
+      Voltage[0][i - 1] = -1 * cumsum_voltage_A / num_samples;
+      Voltage[1][i - 1] = -1 * cumsum_voltage_B / num_samples;
     }
-    if (buff.equals("deaddead")) {
-      Serial.print(Voltage[0]); Serial.print(",");
-      Serial.print(Voltage[1]); Serial.print(",");
-      Serial.print(Voltage[2]); Serial.print(",");
-      Serial.print(Voltage[3]); Serial.print(",\n");
+    if (buff.equals("req")) {
+      Serial.print(Voltage[1][2]); Serial.print(",");
+      Serial.print(Voltage[1][1]); Serial.print(",");
+      Serial.print(Voltage[1][0]); Serial.print(",");
+      Serial.print(Voltage[1][3]); Serial.print(",");
+      Serial.print(Voltage[0][2]); Serial.print(",");
+      Serial.print(Voltage[0][1]); Serial.print(",");
+      Serial.print(Voltage[0][0]); Serial.print(",");
+      Serial.print(Voltage[0][3]); Serial.print(",\n");
     }
   }
 }
