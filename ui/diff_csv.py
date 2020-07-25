6b124940c3948851fb59a74ecbca79c161c59fee
onibrow <seiya.ono12@gmail.com>
2020-07-25 02:12:25 +0000

    update for shipment

--- ui/diff_csv.py
+++ ui/diff_csv.py
@@ -63,13 +63,15 @@ class Diff_ADC():
         self.ser = serial_port
 
     def serial_write(self, data):
+        """
         if sys.platform.startswith('win'):
             self.ser.write([data, ])
         else:
-            self.ser.write(data)
+        """
+        self.ser.write(data)
 
     def serial_request(self):
-        self.serial_write(b'deaddead\n')
+        self.serial_write(b'req\n')
         serial_data = self.ser.readline().decode("utf-8")
         print(serial_data)
         return serial_data
@@ -77,32 +79,36 @@ class Diff_ADC():
     def serial_close(self):
         self.ser.close()
 
+"""
 def rlinput(prompt, prefill=''):
    readline.set_startup_hook(lambda: readline.insert_text(prefill))
    try:
       return input(prompt)
    finally:
       readline.set_startup_hook()
-
+"""
 
 def main():
     print("Starting Quadchannel Differential ADC Data Logger")
     pst = datetime.datetime.now(tz=datetime.timezone.utc).astimezone(timezone('US/Pacific')).strftime("%m-%d-%Y %H:%M")
-    file_name  = rlinput('\nSave data as: \t', 'Diff_ADC_Data {}.csv'.format(pst))
+    # file_name  = rlinput('\nSave data as: \t', 'Diff_ADC_Data {}.csv'.format(pst))
+    file_name = "Diff_ADC_Data {}.csv".format(pst)
+    """
     try:
         delay = int(rlinput('\nSample period in ms (min 300): \t', '300')) / 1000.0
     except ValueError:
         print("\nSample period must be an integer in milliseconds. Exiting...")
         quit()
-    ch1 = rlinput("\nChannel 1 Name: \t", 'Na')
-    ch2 = rlinput("Channel 2 Name: \t", 'K')
-    ch3 = rlinput("Channel 3 Name: \t", 'Unused')
-    ch4 = rlinput("Channel 4 Name: \t", 'NH4')
-
+    """
+    delay = 0.001
+    ch1 = input("\nChannel 1 Name: \t")
+    ch2 = input("Channel 2 Name: \t")
+    ch3 = input("Channel 3 Name: \t")
+    ch4 = input("Channel 4 Name: \t")
     s = select_serial_port()
     mcp = Diff_ADC(s)
     print("\nInitializing MCP3424...")
-    time.sleep(3)
+    time.sleep(2)
     input("\nPress Enter to start, Control+C to stop\n")
     with open(file_name, 'w') as csvfile:
         start_time = time.time()
@@ -110,18 +116,20 @@ def main():
         prev_time = time.time()
         csvfile.write("{}, {}, {}, {},\n".format(ch1, ch2, ch3, ch4))
         while(True):
+            """
             if (time.time() - prev_time > delay):
                 prev_time = time.time()
-                try:
-                    next_line = mcp.serial_request()
-                    csvfile.write(next_line)
-                    i += 1
-                except:
-                    mcp.serial_close()
-                    csvfile.close()
-                    print("\nElapsed Time:   {}".format(time.time() - start_time))
-                    print("Num Samples: {}".format(i))
-                    break
+            """
+            try:
+                next_line = mcp.serial_request()
+                csvfile.write(next_line)
+                i += 1
+            except:
+                mcp.serial_close()
+                csvfile.close()
+                print("\nElapsed Time:   {}".format(time.time() - start_time))
+                print("Num Samples: {}".format(i))
+                break
 
 if __name__ == '__main__':
     main()
